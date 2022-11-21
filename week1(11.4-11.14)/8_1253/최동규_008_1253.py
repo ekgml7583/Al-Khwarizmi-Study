00a8e7ebec9ee6f8503196363e0aff17ee0b3155
on99yu <dgjjanggu@naver.com>
2022-11-21 21:52:35 +0900

    11.21/ 8번 해결

--- week1(11.4-11.14)/8_1253/최동규_008_1253.py
+++ week1(11.4-11.14)/8_1253/최동규_008_1253.py
@@ -7,15 +7,21 @@ NL = sorted(list(map(int, sys.stdin.readline().split())))
 count = 0
 start = 0
 end = 0
-for i in range(N):
-    k = NL[i]
+for k in range(N):
+    find = NL[k]
     start = 0 
     end = N-1
     while start < end:
-        if NL[start] + NL[end] == k:
+        if NL[start] + NL[end] == find: #좋은수를 찾았을경우
             if start!= k and end != k:
-                
-        elif(NL[start] + NL[end]) < k:
-            start +=1
-        else:
-            end =+1
+                count +=1
+                break
+            elif start == k: #시작인덱스가 자기자신일경우
+                start +=1
+            elif end == k: #종료 인덱스가 자기자신일경우
+                end -= 1
+        elif NL[start] + NL[end] < find: # 현재 값이 두수의합보다 클경우
+            start +=1   #수의 합이 작으므로 시작인덱스를 증가
+        else: # 현재 값이 두수의 합보다 작을경우
+            end -=1 # 수의 합이 크므로 종료인덱스를 줄여서 합을 작게하면서 찾음
+print(count)
