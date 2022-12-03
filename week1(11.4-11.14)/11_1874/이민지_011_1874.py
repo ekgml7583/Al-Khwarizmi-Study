76e4788b739a8893b19dffa7c5853bf92043c073
Minji Lee <79428205+alswlfl29@users.noreply.github.com>
2022-12-03 22:42:17 +0900

    11번 풀이
    success로 성공 여부 판단

--- week1(11.4-11.14)/11_1874/이민지_011_1874.py
+++ week1(11.4-11.14)/11_1874/이민지_011_1874.py
@@ -7,27 +7,30 @@ num=[] #수열을 담을 리스트
 for i in range(n):
     num.append(int(input()))  #n개의 수열 입력받기
 
-start=1 #1부터 시작하는 자연수
-s=[] #자연수 넣은 스택
-result=[]
-success=True
+start=1 #자연수 1부터 시작
+stack=[] #자연수를 담을 스택 리스트
+success=True #스택 이용하여 수열 만들 수 있는지 판단
+result=[] #push, pop 출력 리스트
+
 for i in range(n):
-    if num[i]>=start:
+    if num[i]>=start:  #현재 수열 값 >= 자연수 
         while num[i]>=start:
-            s.append(start)
+            stack.append(start) #자연수 1씩 증가하여 스택에 추가
             start+=1
-            result.append("+")
-        s.pop()
+            result.append("+") #push이므로 +
+        stack.pop() #수열 출력하기 위해 마지막 1회만 pop하기
         result.append("-")
-    else:
-        lastpop=s.pop()
-        if lastpop>num[i]:
-            print("No")
-            success=False
+    else: #현재 수열 값 < 자연수
+        last=stack.pop() #스택에 있는 값 꺼내기
+        if last != num[i]: #꺼낸 값이 현재 수열 값이 아닌 경우
+            print("NO")  #NO로 출력
+            success=False #스택 이용하여 수열 만들 수 없음
             break
         else:
             result.append("-")
 
 if success:
-    for i in range(len(result)):
+    for i in range(len(result)):  #result값 출력
         print(result[i])
+
+            
