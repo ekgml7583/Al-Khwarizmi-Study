9f9c064ba3d7607205a6d6308000c80e0433a362
Minji Lee <79428205+alswlfl29@users.noreply.github.com>
2022-12-04 01:14:30 +0900

    10번
    while문

--- week1(11.4-11.14)/10_11003/이민지_010_11003.py
+++ week1(11.4-11.14)/10_11003/이민지_010_11003.py
@@ -1,28 +1,26 @@
+#11003번 최솟값 찾기1
 from collections import deque #deque import
 import sys
 input=sys.stdin.readline
 N, L=map(int,input().split())
 num=list(map(int, input().split()))
-# index=1
 mydeque=deque()
-# for i in range(N):
-#     mydeque.append([index+i,num[i]])
-#     if mydeque[-1][0]-mydeque[0][0]>=3:
-#         mydeque.popleft()
-#     else:
-
-
-for i in range(N):
-    #덱의 마지막 위치에서부터 현재 값보다 큰 값은 덱에서 제거
-    while mydeque and mydeque[-1][0] > num[i]:
+min_list=[]
+mydeque.append((1, num[0])) #첫 번째 노드 넣기
+min_list.append(mydeque[0][1])
+for i in range(1,N):
+    while mydeque[-1][1] >= num[i]: #새로 들어올 노드의 값이 최근 들어간 노드 값보다 작을 경우 최근 들어간 노드 값 빼기(의미 없음)
         mydeque.pop()
-    #덱의 마지막 위치에 현재 값 저장
-    mydeque.append((num[i],i))
-    #덱의 1번째 위치에서부터 L의 범위를 벗어난 값(num index-L<=index)을 덱에서 제거
-    if mydeque[0][1]<=i-L:
-        mydeque.popleft()
-    #덱의 1번째 데이터 출력
-    print(mydeque[0][0], end=' ')
+        if len(mydeque) == 0: #덱이 빈 경우 중단
+            break
+    mydeque.append((i+1,num[i])) #새로 들어온 노드 추가
+    if (i+1)-L >= mydeque[0][0]: #마지막 데이터 index - 슬라이딩 윈도우 크기 >= 맨 앞 노드의 인덱스 값
+        mydeque.popleft() #맨 앞 노드의 인덱스 값 제거
+    min_list.append(mydeque[0][1])
 
+for i in range(len(min_list)):
+    print(min_list[i], end=' ')
+        
 
+    
 
